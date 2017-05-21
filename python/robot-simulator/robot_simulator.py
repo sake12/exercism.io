01225f3eabbdd4f85f95bf04f0d8ae3e19c3ded2
sake12 <sake12@gmail.com>
2017-05-21 12:14:54 +0200

    Robot Simulator

--- python/robot-simulator/robot_simulator.py
+++ python/robot-simulator/robot_simulator.py
@@ -1,32 +1,34 @@
-class NORTH:
-    pass
-
-
-class EAST:
-    pass
-
-
-class SOUTH:
-    pass
-
-
-class WEST:
-    pass
+NORTH, EAST, SOUTH, WEST = 0, 1, 2, 3
 
 
 class Robot(object):
-    def __init__(self, facing=NORTH, pos_x=0, pos_y=0):
+    def __init__(self, bearing=NORTH, pos_x=0, pos_y=0):
         self.coordinates = (pos_x, pos_y)
-        self.bearing = facing
+        self.bearing = bearing
 
     def advance(self):
-        pass
+        coords = self.coordinates
+        if self.bearing == NORTH:
+            self.coordinates = (coords[0], coords[1] + 1)
+        elif self.bearing == EAST:
+            self.coordinates = (coords[0] + 1, coords[1])
+        elif self.bearing == SOUTH:
+            self.coordinates = (coords[0], coords[1] - 1)
+        elif self.bearing == WEST:
+            self.coordinates = (coords[0] - 1, coords[1])
 
     def simulate(self, text):
-        pass
+        for move in text:
+            if move == 'A':
+                self.advance()
+            elif move == 'L':
+                self.turn_left()
+            elif move == 'R':
+                self.turn_right()
 
     def turn_right(self):
-        pass
+        self.bearing = (self.bearing + 1) % 4
 
     def turn_left(self):
-        pass
+        self.bearing = (self.bearing - 1) % 4
+
